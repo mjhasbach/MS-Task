78212dc175372561b6e1850099b4733a6c3557db
Matthew Hasbach <hasbach.git@gmail.com>
2014-03-02 11:57:27 -0500

    Added code

---
+++ tasklist.js
@@ -0,0 +1,145 @@
+var exec = require('child_process').exec,
+    csv = require('csv'),
+    fs = require('fs'),
+    path = require('path'),
+    system32 = 'C:\\Windows\\System32',
+    taskListPath = path.join(system32, 'tasklist.exe'),
+    taskKillPath = path.join(system32, 'taskkill.exe');
+
+// Call tasklist.exe with the supplied arguments - See https://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/tasklist.mspx?mfr=true
+function taskList(arg, callback) {
+    exec(cmdJoin(taskListPath, arg), function(err, stdout) {
+        callback(err, stdout);
+    });
+}
+
+// Search for one or more PIDs that match a give process name
+function pidOf(procName, callback) {
+    if (isString(procName)) {
+        procFilter(procName, function(err, data, amount) {
+            callback(err, data, amount);
+        });
+    } else {
+        var error = 'The first pidOf() argument provided is not a string (not a valid process name).';
+        callback(error, null, null);
+    }
+}
+
+// Search for the process name that corresponds with the supplied PID
+function nameOf(procNumber, callback) {
+    if(isNumber(procNumber)) {
+        procFilter(procNumber, function(err, data) {
+            callback(err, data);
+        });
+    } else {
+        var error = 'The first nameOf() argument provided is non-numeric (not a valid PID).';
+        callback(error, null);
+    }
+}
+
+// Decides whether a process name or number is returned from procStat
+function procFilter(procName, callback) {
+    var index = isString(procName) ? 1 : 0;
+    var result = [];
+
+    procStat(procName, function(err, data, amount) {
+        for (var i = 0; i < amount; i++) {
+            result.push(data.array[i][index]);
+        }
+
+        callback(err, result, amount);
+    });
+}
+
+// Search for a given process name or PID
+function procStat(proc, callback) {
+    var arg = isNumber(proc) ? 'PID' : 'IMAGENAME';
+    var cmd = ' /fi \"' + arg + ' eq ' + proc + '\" /fo CSV';
+    var data = new ProcObject;
+
+    fileExists(taskListPath, function(exists) {
+        if(exists) {
+            taskList(cmd, function(err, stdout) {
+                csv().from.string(stdout).to.array(function(rows){
+                    for (var i = 1; i < rows.length; i++) {
+                        data.array.push(rows[i]);
+                        data.object.push({
+                            name: rows[i][0],
+                            pid: rows[i][1],
+                            sessionName: rows[i][2],
+                            sessionNumber: rows[i][3],
+                            memUsage: rows[i][4]
+                        })
+                    }
+
+                    if (data.length < 1) {
+                        var noun = isNumber(proc) ? 'PIDs' : 'process names';
+                        err = 'There were no ' + noun + ' found when searching for \"' + proc + '\"'
+                    }
+
+                    callback(err, data, data.array.length);
+                });
+            });
+        } else {
+            callback(taskListPath + ' could not be found!', null, null);
+        }
+    });
+}
+
+// Kill a given process name or PID
+function kill(proc, callback) {
+    var arg = isNumber(proc) ? '/PID ' : '/IM ',
+        cmd = 'taskkill /F ' + arg + proc,
+        err = null;
+
+    if (isNumber(proc) || isString(proc)) {
+        fileExists(taskKillPath, function(exists) {
+            if (exists) {
+                exec(cmd, function(err) {
+                    callback(err);
+                });
+            } else {
+                err = taskListPath + ' could not be found!';
+                callback(err)
+            }
+        })
+    } else {
+        err = 'The first kill() argument provided is neither a string nor an integer (not a valid process).';
+        callback(err)
+    }
+}
+
+// Check if a file exists
+function fileExists(path, callback) {
+    fs.exists(path, function(exists) {
+        callback(exists)
+    });
+}
+
+// Append parameters to a command
+function cmdJoin(cmd, params) {
+    params = typeof params == 'undefined' ? '' : ' ' + params;
+    return cmd + params
+}
+
+// The object which is returned from procStat
+function ProcObject() {
+    this.array = [];
+    this.object = [];
+}
+
+// Check if an object is a string
+function isString(obj) {
+    return Object.prototype.toString.call(obj) == '[object String]'
+}
+
+// Check if an object is a number
+function isNumber(obj) {
+    return /^\d+$/.test(obj)
+}
+
+module.exports = taskList;
+module.exports.procStat = procStat;
+module.exports.pidOf = pidOf;
+module.exports.nameOf = nameOf;
+module.exports.kill = kill;
---
+++ test/test.js
@@ -0,0 +1,104 @@
+// Before running this test, be sure to:
+// cd test/
+// npm install
+// This will download the async library, which is required for the test, but not MS-Task itself
+
+var tasklist = require('../tasklist'),
+    async = require('async');
+
+async.parallel([
+    function(callback){
+        tasklist('/fi "IMAGENAME eq explorer.exe" /fo CSV', function(err, data) {
+            if(err) throw err;
+            console.log('tasklist test');
+            console.log(data);
+
+            callback(null, null);
+        });
+    },
+    function(callback){
+        tasklist.procStat('explorer.exe', function(err, data, amount) {
+            if(err) throw err;
+            console.log('tasklist.procStat test');
+
+            if(amount > 0) {
+                console.log('One or more matching processes were found, here they are:');
+
+                for (var x = 0; x < amount; x++) {
+                    console.log('(From data.array)');
+                    console.log('Process Name - ' + data.array[x][0]);
+                    console.log('Process Number - ' + data.array[x][1]);
+                    console.log('Session Name - ' + data.array[x][2]);
+                    console.log('Session Number - ' + data.array[x][3]);
+                    console.log('Memory Usage - ' + data.array[x][4]);
+                }
+
+                for (var y = 0; y < amount; y++) {
+                    console.log('(From data.object)');
+                    console.log('Process Name - ' + data.object[y].name);
+                    console.log('Process Number - ' + data.object[y].pid);
+                    console.log('Session Name - ' + data.object[y].sessionName);
+                    console.log('Session Number - ' + data.object[y].sessionNumber);
+                    console.log('Memory Usage - ' + data.object[y].memUsage);
+                }
+            } else {
+                console.log("If Explorer isn't running on your PC, I can't help you :)")
+            }
+
+            console.log('');
+
+            callback(null, null);
+        });
+    },
+    function(callback){
+        tasklist.pidOf('explorer.exe', function(err, data, amount) {
+            if(err) throw err;
+            console.log('tasklist.pidOf test');
+
+            if(amount > 0) {
+                console.log('One or more matching process numbers were found, here they are:');
+
+                for (var i = 0; i < amount; i++) {
+                    console.log(data[i]);
+                }
+
+                console.log('');
+            } else {
+                console.log("If Explorer isn't running on your PC, I can't help you :)")
+            }
+
+            callback(null, null);
+        });
+    },
+    function(callback){
+        tasklist.nameOf(0, function(err, data) {
+            console.log('tasklist.nameOf test');
+
+            if(err) {
+                console.log("If System Idle Process isn't running on your PC, I can't help you :)")
+            }
+            else {
+                console.log('A matching process name was found, here it is: ' + data);
+            }
+
+            console.log('');
+            callback(null, null);
+        });
+    }
+],
+
+// Commit suicide when all of the above tasks have completed
+function(){
+    console.log("tasklist.kill test (committing suicide)");
+    tasklist.kill(process.pid, function(){});
+    // Exit code will be 1 if the suicide was successful
+});
+
+
+
+
+
+
+
+
+
